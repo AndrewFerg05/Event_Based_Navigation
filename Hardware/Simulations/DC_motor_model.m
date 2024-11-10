f4996ee54694ab95e3d2530f752dac41d690c1e0
Douglas02 <douglasfraser15@icloud.com>
2024-11-10 15:52:57 +0000

    made simulink model and started control design.

--- Hardware/Simulations/DC_motor_model.m
+++ Hardware/Simulations/DC_motor_model.m
@@ -8,6 +8,8 @@ Km = 0.1;               % torque constant
 Kb = 0.1;               % back emf constant
 Kf = 0.2;               % Nms
 J = 0.02;               % kg.m^2/s^2
+Gr = 1/35;                % gear reduction ratio of the motor
+r = 32.5e-3;            % radius of wheel
 
 %% systems model set up 
 
@@ -20,6 +22,13 @@ dcm = feedback(dcm,Kb,1,1);   % close back emf loop
 % Extract transfer functions for each input-output path
 dcm_tf = tf(dcm);            % Convert state-space model to transfer function form
 
+zpk(dcm_tf);
+
+%% plant inluding gear ratio and output linear velocity
+
+plant = dcm*Gr*r;
+
+zpk(plant)
 zpk(dcm_tf)
 
 %% system analysis 

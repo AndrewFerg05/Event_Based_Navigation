76f66e343f909400cde61e4635d747440aa40d29
AndrewFerg05 <andrewferguson8@zoho.com>
2025-02-15 12:37:32 +0000

    Revert "Merge branch 'Clean_Synch' into software_dev_sark"
    This reverts commit 70c314bb117e5cb179d7260531ddfc83813b987e, reversing
    changes made to 79ec162288ab541b1397129162b3c956336b1d17.

--- Software/utils/common/include/Flags.hpp
+++ Software/utils/common/include/Flags.hpp
@@ -31,17 +31,6 @@ constexpr int VIO_CERES_SLIDING_WINDOW_SIZE = 3;
 constexpr int VIO_CERES_NUM_KEYFRAMES = 5;
 constexpr int VIO_CERES_NUM_THREADS = 1;
 
-// Depth thresholds
-constexpr double VIO_MIN_DEPTH = 0.1;  // Minimum depth for VIO
-constexpr double VIO_MAX_DEPTH = 10.0; // Maximum depth for VIO
-constexpr double VIO_MEDIAN_DEPTH = 1.0; // Median depth for VIO
-
-// Feature tracking limits
-constexpr uint64_t VIO_MIN_TRACKED_FEATURES_TOTAL = 50;  // Min tracked features globally
-constexpr uint64_t VIO_MAX_TRACKED_FEATURES_PER_FRAME = 150; // Max features per frame
-
-// Processing mode
-constexpr bool VIO_DELAYED_NFRAME_PROCESSING = false; // Enable or disable delayed processing
 
 inline bool FLAGS_data_use_time_interval = 0;
 inline uint64_t FLAGS_data_interval_between_event_packets = 15000; //number of frames in event packet/ ms between packets set which by above flag

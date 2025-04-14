ae5a543841218808912276f13d508829e8c0fc6a
AndrewFerg05 <andrewferguson8@zoho.com>
2025-04-14 13:18:14 +0100

    actually adding yaml

--- Software/include/FrontEnd.hpp
+++ Software/include/FrontEnd.hpp
@@ -32,6 +32,18 @@ Change History
 //==============================================================================
 //      Classes
 //------------------------------------------------------------------------------
+struct FrameProcessingParams {
+    // Frame Building flags to ignore stationary or extreme event rates
+    int32_t noise_event_rate = static_cast<int32_t>(0.11e+06);         // Minimum event rate below which ignored
+    int32_t event_ignore_threshold = static_cast<int32_t>(6e+06);      // Maximum event rate above which ignored
+
+    // Frame Blending flags
+    int32_t max_event_rate = static_cast<int32_t>(0.18e+06);           // Level at which Maximum blend is used
+    double max_event_blend = 0.4;                                      // Maximum blend amount
+    double min_event_blend = 0.2;                                      // Minimum blend amount
+};
+
+
 class FrontEnd
 {
     public:
@@ -67,6 +79,7 @@ class FrontEnd
 
     private:
     FrameType frame_config_ = COMBINED_FRAME;
+    FrameProcessingParams params;
     std::atomic<bool> stateInitialised_{false};
     std::atomic<bool> vioReady_{false};
     std::shared_ptr<CommunicationManager> comms_interface_;
